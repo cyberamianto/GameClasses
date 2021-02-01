71f75e4458e7d6bbf1caa2d0c3e7c2049539774d
Giammarco <giammarco.perugini@gmail.com>
2021-02-01 17:35:51 +0100

    gameclasses

--- GameClasses/NPC.cs
+++ GameClasses/NPC.cs
@@ -7,12 +7,12 @@ namespace VideoGame
 {
     public class NPC : Personaggio
     {
-        public NPC(string nome, int salute, int exp, string desc, int amb) : base(nome, salute, exp)
+        public NPC(string nome, int salute, int exp, string desc, string amb) : base(nome, salute, exp)
         {
             Descrizione = desc;
             Amb = amb;
         }
         string Descrizione { get; set; }
-        int Amb { get; set; }
+        string Amb { get; set; }
     }
 }
