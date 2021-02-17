fdf287770e998156a58d68af6f3f65522f6cc7ea
Giammarco <giammarco.perugini@gmail.com>
2021-02-17 16:15:48 +0100

    added_frase

--- GameClasses/NPC.cs
+++ GameClasses/NPC.cs
@@ -7,12 +7,14 @@ namespace VideoGame
 {
     public class NPC : Personaggio
     {
-        public NPC(string nome, string salute, int exp, string desc, string amb) : base(nome, salute, exp)
+        public NPC(string nome, string salute, int exp, string desc, string amb, string frase) : base(nome, salute, exp)
         {
             Descrizione = desc;
             Amb = amb;
+            Frase = frase;
         }
         string Descrizione { get; set; }
         string Amb { get; set; }
+        string Frase { get; set; }
     }
 }
